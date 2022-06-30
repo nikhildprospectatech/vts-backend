15c23a43ccdb16b988db4af0cf7568277300f256
Nikhil Desai <nikhild@prospectatech.local>
2022-06-30 11:28:07 +0530

    changes

--- apiGetCurrentVehicles.js
+++ apiGetCurrentVehicles.js
@@ -1,19 +1,17 @@
 const DATABASE = require("./connection/db_connection")
 const {DBNAME, VEHICLEDATA} = require("./constant/database")
 
-exports.apiGetData = async (req,res) => {
+exports.apiGetCurrentVehicles = async (req,res) => {
     console.log('API Get data triggered')
-    let findBy = {};
+    let findBy = {currentStatus : JSON.parse(req.query.currentStatus)};
 
     try{
 
         let client = await DATABASE.getClient();
         let response = await client.db(DBNAME).collection(VEHICLEDATA).find(findBy).toArray();
-        console.log(response)
         res.status(200).send(response);
     }
     catch(err){
         throw new Error(err.toString());
     }
 };
-
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,12 @@ const express = require("express");
 
 var router = express.Router();
 
-const {apiGetData} = require('../apiGetData');
-const {apiPostData} = require('../apiPostData');
+const {apiGetCurrentVehicles} = require('../apiGetCurrentVehicles');
 const {apiGetVehicleData} = require('../apiGetVehicleData');
 const {apiPostVehicleData} = require('../apiPostVehicleData');
 
-router.post('/postSample',apiPostData)
-router.get("/getData", apiGetData)
+
+router.get("/apiGetCurrentVehicles", apiGetCurrentVehicles)
 
 router.post('/postVehicleData', apiPostVehicleData)
 router.get('/getVehicleData', apiGetVehicleData)

c6ad8105b5d6ac420254cb83e784a74ea9dabfc8
Nikhil Desai <nikhild@prospectatech.local>
2022-07-26 12:29:49 +0530

    bug

--- apiCheckOut.js
+++ apiCheckOut.js
@@ -3,15 +3,23 @@ const {DBNAME, VEHICLEDATA} = require("./constant/database")
 
 exports.apiCheckOutData = async (req,res) => {
     let findBy = {vehicleNumber : req.body.vehicleNumber};
+    let findById = {};
     try{
      
 
         let client = await DATABASE.getClient();
+        const filterData = await client.db(DBNAME).collection(VEHICLEDATA).find(findBy).toArray();
+
+        filterData.forEach(ele => {
+            if(!ele.exitDate){
+                findById['vehicleId'] = ele.vehicleId;
+            }
+        });
 
         let payload = { exitDate: Math.floor(new Date().getTime() / 1000), exitTime: new Date().toLocaleTimeString('en-US', {  hour: '2-digit', minute: '2-digit', hours12 : false}), currentStatus: false} 
 
         const result = await client.db(DBNAME).collection(VEHICLEDATA).findOneAndUpdate(
-            findBy,
+            findById,
             { $set : payload }
         )
 
